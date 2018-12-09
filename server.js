e704cb9370863a62e273fd8c70ce75ccd0334227
Tara Fenton <tarafenton@Taras-MBP-3.fios-router.home>
2018-12-09 14:30:49 -0500

    get places routes

--- server.js
+++ server.js
@@ -2,7 +2,44 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 
+const { Place } = require('./models');
 const app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to the Place Tracker API'
+  });
+});
+
+app.get('/places', async (req, res) => {
+  try {
+    const places = await Place.findAll({});
+    res.json(places);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: e.message});
+  }
+});
+
+app.get('/places/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const place = await Place.findByPk(id);
+    res.json(place);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: e.message});
+  }
+});
+
+app.post('/places', async (req, res) => {
+  try {
+    //post
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: e.message});
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
