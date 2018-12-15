09d689593b0d46c4d0e86fdc7f7eafb9e0b0dba4
Tara Fenton <tarafenton@Taras-MBP-3.fios-router.home>
2018-12-15 12:24:25 -0500

    add auth dependencies and routes

--- server.js
+++ server.js
@@ -2,64 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
+const bcrypt = require('bcrypt');
+const { passport, sign } = require('./auth');
+const { Place, User } = require('./models');
+
+const { placesRouter } = require('./routes/places');
+const { usersRouter } = require('./routes/users');
 
 const PORT = process.env.PORT || 3001;
-
-const { Place } = require('./models');
 const app = express();
 
 app.use(logger('dev'));
 app.use(cors());
 app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Welcome to the Place Tracker API'
-  });
-});
-
-app.get('/places', async (req, res) => {
-  try {
-    const places = await Place.findAll({});
-    res.json(places);
-  } catch (e) {
-    console.error(e);
-    res.status(500).json({ message: e.message});
-  }
-});
-
-app.get('/places/:id', async (req, res) => {
-  try {
-    const id = req.params.id;
-    const place = await Place.findByPk(id);
-    res.json(place);
-  } catch (e) {
-    console.error(e);
-    res.status(500).json({ message: e.message});
-  }
-});
-
-app.delete('/places/:id', async (req, res) => {
-  try {
-    const id = req.params.id;
-    const place = await Place.findByPk(id);
-    if (place) await place.destroy();
-    res.json({ message: `Student with id ${id} deleted`});
-  } catch (e) {
-    console.error(e);
-    res.status(500).json({ message: e.message});
-  }
-});
-
-app.post('/places', async (req, res) => {
-  try {
-    const place = await Place.create(req.body);
-    res.json(place);
-  } catch (e) {
-    console.error(e);
-    res.status(500).json({ message: e.message});
-  }
-});
+app.use('/places', placesRouter);
+app.use('/users', usersRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
