4548bf9fd6d9ddfc5c5af851063040f45d93d1b8
Tara Fenton <tarafenton@Taras-MBP-3.fios-router.home>
2018-12-15 12:20:50 -0500

    add user to seed

--- seed.js
+++ seed.js
@@ -1,37 +1,60 @@
-const { Place } = require('./models');
+const { Place, User } = require('./models');
+
+async function createPlaces() {
+  await User.destroy({ where: {} });
+  try {
+    const places = await Place.bulkCreate([
+      {
+        name: 'La Cabana',
+        description: 'tropical island',
+        visited: true,
+        address: 'Aruba',
+      },
+      {
+        name: 'Empire State Building',
+        description: 'skyscrapper',
+        visited: true,
+        address: '350 5th Ave New York, NY',
+      },
+      {
+        name: 'Howl at the Moon',
+        description: 'saloon',
+        visited: false,
+        address: 'New Orleans',
+      },
+    ]);
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+async function createUsers() {
+  await User.destroy({ where: {} });
+  try {
+    const user = await User.bulkCreate([
+      {
+        username: 'tara',
+        password: 'tara',
+      },
+      {
+        username: 'john',
+        password: 'john',
+      }
+    ]);
+  } catch(e) {
+    console.log(e);
+  }
+}
 
 async function seed() {
-  const places = await Place.bulkCreate([
-    {
-      name: 'La Cabana',
-      description: 'tropical island',
-      visited: true,
-      address: 'Aruba',
-    },
-    {
-      name: 'Empire State Building',
-      description: 'skyscrapper',
-      visited: true,
-      address: '350 5th Ave New York, NY',
-    },
-    {
-      name: 'Howl at the Moon',
-      description: 'saloon',
-      visited: false,
-      address: 'New Orleans',
-    },
-  ]);
-  const users = await User.bulkCreate([
-    {
-      username: 'tara',
-      password: 'tara',
-    },
-    {
-      username: 'john',
-      password: 'john',
-    }
-  ]);
-  process.exit()
+  try {
+    await createPlaces();
+    await createUsers();
+  } catch (e) {
+    console.log(e);
+  } finally {
+    process.exit();
+  }
 }
 
 seed();
